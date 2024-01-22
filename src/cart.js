fa0e742fa08edc835184119cecac3f7c9a64e671
haleomo <rob99202@gmail.com>
2024-01-21 16:00:58 -1000

    Added code to increment cart quantity and remove unwanted items from the cart

--- src/cart.js
+++ src/cart.js
@@ -21,7 +21,7 @@ function expandCart() {
       cartDiv.style.visibility = "hidden";
       cartDiv.style.height = "0px";
     });
-    
+
   document
     .getElementById("checkout-btn")
     .addEventListener("click", function () {
@@ -30,19 +30,55 @@ function expandCart() {
     });
 }
 
+function incrementQuantityValue(spanId) {
+  quantitySpan = document.getElementById(spanId);
+  var value = Number(quantitySpan.innerHTML);
+  quantitySpan.innerHTML = (isNaN(value) ? 0 : value + 1).toString();
+}
+
+function decrementQuantityValue(spanId) {
+  quantitySpan = document.getElementById(spanId);
+  var value = Number(quantitySpan.innerHTML);
+  quantitySpan.innerHTML = (
+    isNaN(value) || value < 1 ? 0 : value - 1
+  ).toString();
+}
+
+/*
+
+params:
+  cartItem -- is an element in the object cartItems, referenced by the productId. 
+              it's used in a loop that sets all the items displayed in the cart.
+*/
 function createCartItem(cartItem, counter) {
   productItem = seeds.products.find(
     (obj) => obj.productTag === cartItem.product
   );
   console.log(productItem);
+  var itemQuantitySpanId = `itemQuantity-${counter}`;
+  var cartItemListItemId = `${productItem.productName}-listId`;
 
   cartDiv = document.getElementById("shopping-cart-items");
   cartItemListItem = document.createElement("li");
+  cartItemListItem.id = cartItemListItemId;
   cartItemSpan = document.createElement("span");
   buttonsSpan = document.createElement("span");
   buttonsSpan.className = "cart-buttons-span";
+   
   buttonDeleteSpan = document.createElement("icon");
   buttonDeleteSpan.innerHTML = '<i class="fa fa-trash fa-lg"><i>';
+  buttonDeleteSpan.addEventListener("click", (event) => {
+    var shoppingCartIcon = document.getElementById("shopping-cart-icon");
+    delete cartItems[cartItem.product];
+    var value = Number(shoppingCartIcon.getAttribute("value"));
+    console.log(`Cart Items: ${cartItems}`);
+    shoppingCartIcon.setAttribute(
+      "value",
+      value > cartItem.quantity ? value - cartItem.quantity : 0
+    );
+    document.getElementById(cartItemListItemId).remove();
+  });
+
   buttonsSpan.appendChild(buttonDeleteSpan);
   buttonLike = document.createElement("icon");
   buttonLike.innerHTML = '<i class="fa fa-heart fa-lg"><i>';
@@ -54,24 +90,33 @@ function createCartItem(cartItem, counter) {
   cartItemSpan.appendChild(imageElement);
   description = document.createElement("span");
   description.className = "item-description";
-  console.log(`Product Name ${productItem.productName}  ${cartItem.quantity}`);
+  console.log(
+    `Product Name: ${productItem.productName}  Quantity: ${cartItem.quantity}`
+  );
   description.innerHTML = productItem.productName;
   cartItemSpan.appendChild(description);
 
   quantitySpan = document.createElement("span");
   buttonPlus = document.createElement("button");
+  buttonPlus.id = `${productItem}-plus-button`;
   buttonPlus.className = "item-quantity-button";
   buttonPlus.innerHTML = "+";
-  buttonPlus.addEventListener("click", function (event) {});
+  buttonPlus.addEventListener("click", function (event) {
+    incrementQuantityValue(itemQuantitySpanId);
+  });
   quantitySpan.appendChild(buttonPlus);
   quantityValueSpan = document.createElement("span");
   quantityValueSpan.className = "item-quantity";
   quantityValueSpan.innerHTML = cartItem.quantity;
-  quantityValueSpan.id = `itemQuantity-${counter}`;
+  quantityValueSpan.id = itemQuantitySpanId;
   quantitySpan.appendChild(quantityValueSpan);
   buttonMinus = document.createElement("button");
+  buttonMinus.id = `${productItem}-minus-button`;
   buttonMinus.className = "item-quantity-button";
   buttonMinus.innerHTML = "-";
+  buttonMinus.addEventListener("click", function (event) {
+    decrementQuantityValue(itemQuantitySpanId);
+  });
   quantityValueSpan.className = "item-quantity";
   quantitySpan.appendChild(buttonMinus);
 
@@ -79,5 +124,4 @@ function createCartItem(cartItem, counter) {
 
   cartItemListItem.appendChild(cartItemSpan);
   return cartItemListItem;
-  //cartDiv.appendChild(cartItemListItem);
 }
